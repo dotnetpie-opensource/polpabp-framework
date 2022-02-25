ab8c44be1e3ce82b4fa86e1debe10330ce486fc8
dotnetpie <polpabp@gmail.com>
2022-02-25 06:43:40 -0500

    Improve permission and localization design

--- src/PolpAbp.Framework.Core.Shared/Authorization/FrameworkPermissions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PolpAbp.Framework.Authorization
-{
-    public static class FrameworkPermissions
-    {
-        // Multi org unit permissions
-        public const string Pages_Administration_OrganizationUnits_ManagePermissions = "Pages.Administration.OrganizationUnits.ManagePermissions";
-    }
-}
