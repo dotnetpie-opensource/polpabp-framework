8cd52d3d969f2117e9848f71c8f526bfb5fd3cd2
dotnetpie <polpabp@gmail.com>
2022-03-01 10:55:03 -0500

    Allow operatorId to pass throughout the background flow

--- src/PolpAbp.Framework.Application.Contracts/BackgroundJobs/IndividualTenantJobArgs.cs
+++ src/PolpAbp.Framework.Application.Contracts/BackgroundJobs/IndividualTenantJobArgs.cs
@@ -5,5 +5,7 @@ namespace PolpAbp.Framework.BackgroundJobs
     public abstract class IndividualTenantJobArgs
     {
         public Guid TenantId { get; set; }
+
+        public Guid? OperatorId { get; set; }
     }
 }
