6265a8ffa5f4e9d10afb1f2fde978091989e9482
dotnetpie <polpabp@gmail.com>
2022-03-30 17:04:50 -0400

    Support finding out the members for a role

--- src/PolpAbp.Framework.Domain/Identity/IIdentityUserRepositoryExt.cs
+++ src/PolpAbp.Framework.Domain/Identity/IIdentityUserRepositoryExt.cs
@@ -10,7 +10,11 @@ namespace PolpAbp.Framework.Identity
     public interface IIdentityUserRepositoryExt : IBasicRepository<IdentityUser, Guid>
     {
         Task<long> CountUsersInOrganizationUnitAsync(Guid organizationUnitId, string filter = null, CancellationToken cancellationToken = default);
+        Task<long> CountUsersInRoleAsync(Guid RoleId, string filter = null, CancellationToken cancellationToken = default);
+        Task<long> CountUsersNotInRoleAsync(Guid RoleId, string filter = null, CancellationToken cancellationToken = default);
         Task<List<IdentityUser>> GetListAsync(Guid[] ids, bool includeDetails = false, CancellationToken cancellationToken = default);
         Task<List<IdentityUser>> GetUsersInOrganizationUnitAsync(Guid organizationUnitId, string sorting = null, int maxResultCount = int.MaxValue, int skipCount = 0, string filter = null, bool includeDetails = false, CancellationToken cancellationToken = default);
+        Task<List<IdentityUser>> GetUsersInRolAsync(Guid RoleId, string sorting = null, int maxResultCount = int.MaxValue, int skipCount = 0, string filter = null, bool includeDetails = false, CancellationToken cancellationToken = default);
+        Task<List<IdentityUser>> GetUsersNotInRolAsync(Guid RoleId, string sorting = null, int maxResultCount = int.MaxValue, int skipCount = 0, string filter = null, bool includeDetails = false, CancellationToken cancellationToken = default);
     }
 }
