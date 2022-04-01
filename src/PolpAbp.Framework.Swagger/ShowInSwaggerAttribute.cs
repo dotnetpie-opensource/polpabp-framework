c5dde81e0df3c96e5e6d078fbbbedd1d7adeef8c
dotnetpie <polpabp@gmail.com>
2022-04-01 17:33:45 -0400

    Swagger support module

---
+++ src/PolpAbp.Framework.Swagger/ShowInSwaggerAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Linq;
+
+namespace PolpAbp.Framework
+{     
+    public class ShowInSwaggerAttribute : Attribute, IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            var filteredApis = context.ApiDescriptions.Where(a => a.CustomAttributes().Any(x =>
+                x.GetType() == typeof(ShowInSwaggerAttribute)));
+
+            foreach (var path in swaggerDoc.Paths.ToList())
+            {
+                if (filteredApis.All(x => ("/" + x.RelativePath) != path.Key))
+                    swaggerDoc.Paths.Remove(path.Key);
+            }
+        }
+    }
+}
