786f750cccd5bd6e8cbf2673c3666fc65a6d93c1
dotnetpie <tom@fake.com>
2022-08-31 16:26:52 -0400

    Script

--- src/PolpAbp.Framework.Application.Contracts/Emailing/Account/IFrameworkAccountEmailer.cs
+++ src/PolpAbp.Framework.Application.Contracts/Emailing/Account/IFrameworkAccountEmailer.cs
@@ -5,6 +5,12 @@ namespace PolpAbp.Framework.Emailing.Account
 {
     public interface IFrameworkAccountEmailer
     {
+        /// <summary>
+        /// Sends out the activation email to confirm that the email exists
+        /// or to activate the account depending on the usage of the host.
+        /// </summary>
+        /// <param name="email">Account email</param>
+        /// <returns>Task</returns>
         Task SendEmailActivationLinkAsync(string email);
         Task SendPasswordChangeNotyAsync(Guid userId);
     }
