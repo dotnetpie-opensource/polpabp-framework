96aca1fd00075859aca34dad40b825be06ded73b
dotnetpie <tom@fake.com>
2022-09-02 18:28:40 -0400

    Settings

---
+++ src/PolpAbp.Framework.Domain/Settings/FrameworkSettingDefinitionProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Settings;
+
+namespace PolpAbp.Framework.Settings
+{
+    public class FrameworkSettingDefinitionProvider : SettingDefinitionProvider
+    {
+        public override void Define(ISettingDefinitionContext context)
+        {
+            // Account 
+            context.Add(new SettingDefinition(FrameworkSettings.TenantAccountRegistrationApprovalType));
+            context.Add(new SettingDefinition(FrameworkSettings.TenantAccountRegistrationDisabled));
+        }
+    }
+}
+
---
+++ src/PolpAbp.Framework.Domain/Settings/FrameworkSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PolpAbp.Framework.Settings
+{
+    public static class FrameworkSettings
+    {
+        public const string GroupName = "PolpAbp.Framework";
+
+        // Account 
+        public const string AccountSubGroup = $"{GroupName}.Account";
+
+        public const string TenantAccountRegistrationDisabled = $"{AccountSubGroup}.Registeration.Disabled";
+        // Email (0), Admin (1), Auto (2) ...
+        public const string TenantAccountRegistrationApprovalType = $"{AccountSubGroup}.Registeration.ApprovalType";
+    }
+}
