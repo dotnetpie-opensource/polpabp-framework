a0728123d487cc217eb55d817391aabdfd4a71ba
Tom Tang <polpware@gmail.com>
2022-09-02 07:39:31 -0400

    Configure multi tenancy

--- src/PolpAbp.Framework.AspNetCore/FrameworkAspNetCoreModule.cs
+++ src/PolpAbp.Framework.AspNetCore/FrameworkAspNetCoreModule.cs
@@ -1,4 +1,5 @@
 ﻿using Volo.Abp.AspNetCore;
+using Volo.Abp.AspNetCore.MultiTenancy;
 using Volo.Abp.Identity.AspNetCore;
 using Volo.Abp.IdentityServer;
 using Volo.Abp.Modularity;
@@ -8,7 +9,8 @@ namespace PolpAbp.Framework
     [DependsOn(
         typeof(AbpAspNetCoreModule),
         typeof(AbpIdentityAspNetCoreModule), 
-        typeof(AbpIdentityServerDomainModule)
+        typeof(AbpIdentityServerDomainModule),
+        typeof(AbpAspNetCoreMultiTenancyModule)
         )]
     public class FrameworkAspNetCoreModule : AbpModule
     {
