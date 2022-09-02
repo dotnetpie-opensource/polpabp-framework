7fcdac38f0b3a314bd0724f1667bb4d74db6f556
Tom Tang <polpware@gmail.com>
2022-09-02 07:39:13 -0400

    Customized multi tenancy middleware

---
+++ src/PolpAbp.Framework.AspNetCore/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using PolpAbp.Framework.CustomServices;
+
+namespace PolpAbp.Framework.AspNetCore
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Use cutomized one to have extra featurs.
+        /// </summary>
+        /// <returns></returns>
+        public static IApplicationBuilder UseCustomMultiTenancy(this IApplicationBuilder app)
+        {
+            return app
+                .UseMiddleware<CustomMultiTenancyMiddleware>();
+        }
+
+    }
+}
