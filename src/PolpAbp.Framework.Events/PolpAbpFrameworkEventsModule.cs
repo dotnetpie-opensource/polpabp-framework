df2308df5095552c7e1836cb85a29ae38dab6d15
Tom Tang <polpware@gmail.com>
2022-09-10 16:26:27 -0400

    Factor events into a separate project

---
+++ src/PolpAbp.Framework.Events/PolpAbpFrameworkEventsModule.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.EventBus;
+using Volo.Abp.Modularity;
+
+namespace PolpAbp.Framework
+{
+    [DependsOn(
+        typeof(AbpEventBusModule)
+    )]
+    public class PolpAbpFrameworkEventsModule : AbpModule
+    {
+    }
+}
