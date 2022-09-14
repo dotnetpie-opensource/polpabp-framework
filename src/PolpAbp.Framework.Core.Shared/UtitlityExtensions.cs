9b79aba85cb59c6fe3617f36297070cbeaa8f1c7
dotnetpie <tom@fake.com>
2022-09-14 17:06:55 -0400

    Extensions

---
+++ src/PolpAbp.Framework.Core.Shared/UtitlityExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PolpAbp.Framework
+{
+    public static class UtitlityExtensions
+    {
+        public static string MaskEmailAddress(this string input)
+        {
+            string pattern = @"(?<=[\w]{1})[\w-\._\+%]*(?=[\w]{1}@)";
+            string result = Regex.Replace(input, pattern, m => new string('*', m.Length));
+            return result;
+        }
+
+        public static string MaskPhoneNumber(this string str)
+        {
+            var temp = Regex.Replace(str, "[0-9]", "*");
+            str = temp + str.Substring(str.Length - 4, str.Length - (str.Length - 4));
+            return str;
+        }
+    }
+}
+
