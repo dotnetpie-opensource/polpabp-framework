ec5e2cbde63f9a9b2911cef9cb40883156f4a182
dotnetpie <tom@fake.com>
2022-10-04 15:41:57 -0400

    More utility function

--- src/PolpAbp.Framework.Core.Shared/UtitlityExtensions.cs
+++ src/PolpAbp.Framework.Core.Shared/UtitlityExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace PolpAbp.Framework
 {
@@ -22,6 +23,59 @@ namespace PolpAbp.Framework
         {
             return string.IsNullOrEmpty(firstName) ? $"{lastName ?? deaultValue}"
                 : $"{firstName} {lastName ?? ""}";
+        }
+
+        /// <summary>
+        /// Parses the given string into a pair of first name and last name,
+        /// or uses the given first name and last name.
+        ///
+        /// Usage:
+        /// ParseFullName("tan,rong", null, null, true, ',');
+        /// ParseFullName("Rong Tan", null, null, false, ' ');
+        /// </summary>
+        /// <param name="fullName">Full name</param>
+        /// <param name="firstName">Default first name</param>
+        /// <param name="lastName">Default last name</param>
+        /// <param name="firstNameLeading">Last name leads in the given fullName or not</param>
+        /// <param name="separator">The separator default to be ,</param>
+        /// <returns>A pair of names</returns>
+        public static Tuple<string, string> ParseFullName(this string fullName,
+            string firstName = null, string lastName = null,
+            bool firstNameLeading = false, char separator=',')
+        {
+            // If first name or last name is provider, use them directly.
+            if (!string.IsNullOrEmpty(firstName) || !string.IsNullOrEmpty(lastName))
+            {
+                firstName = firstName ?? "Name";
+                lastName = lastName ?? "Surname";
+                return new Tuple<string, string>(firstName, lastName);
+            }
+
+            // fullName has value.
+            if (!string.IsNullOrEmpty(fullName))
+            {
+                fullName = fullName.Trim();
+
+                var idx = fullName.IndexOf(separator);
+                if (idx != -1)
+                {
+                    if (firstNameLeading)
+                    {
+                        lastName = fullName.Substring(0, idx)?.Trim();
+                        firstName = fullName.Substring(idx + 1)?.Trim();
+                    }
+                    else
+                    {
+                        firstName = fullName.Substring(0, idx)?.Trim();
+                        lastName = fullName.Substring(idx + 1)?.Trim();
+                    }
+                }
+            }
+
+            firstName = firstName ?? "Name";
+            lastName = lastName ?? "Surname";
+
+            return new Tuple<string, string>(firstName, lastName);
         }
     }
 }
