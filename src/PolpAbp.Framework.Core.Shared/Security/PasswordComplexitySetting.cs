db47e5619df7ce156708be88c87958dd0ec35b12
dotnetpie <tom@fake.com>
2022-10-17 18:05:28 -0400

    Factor in some password service from zero adptor

---
+++ src/PolpAbp.Framework.Core.Shared/Security/PasswordComplexitySetting.cs
@@ -0,0 +1,39 @@
+﻿namespace PolpAbp.Framework.Security
+{
+    public class PasswordComplexitySetting
+    {
+        public static PasswordComplexitySetting DefaultSettings = new PasswordComplexitySetting()
+        {
+            RequireDigit = true,
+            RequireLowercase = true,
+            RequiredLength = 8,
+            RequireNonAlphanumeric = true,
+            RequireUppercase = true
+        };
+
+        public bool Equals(PasswordComplexitySetting other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return
+                RequireDigit == other.RequireDigit &&
+                RequireLowercase == other.RequireLowercase &&
+                RequireNonAlphanumeric == other.RequireNonAlphanumeric &&
+                RequireUppercase == other.RequireUppercase &&
+                RequiredLength == other.RequiredLength;
+        }
+
+        public bool RequireDigit { get; set; }
+
+        public bool RequireLowercase { get; set; }
+
+        public bool RequireNonAlphanumeric { get; set; }
+
+        public bool RequireUppercase { get; set; }
+
+        public int RequiredLength { get; set; }
+    }
+}
