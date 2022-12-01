3f9760db93cfe2868bd6aabf29ead1bc3e28fc0a
X. Tang <x.long@infobeyondtech.com>
2022-12-01 15:42:08 -0500

    A new module for the common extensions to the abp entityframeworkcore

---
+++ src/PolpAbp.Extensions.EntityFrameworkCore/PolpAbpEfCoreRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Volo.Abp.Domain.Entities;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+
+public class PolpAbpEfCoreRepository<TDbContext, TEntity> : EfCoreRepository<TDbContext, TEntity>
+    where TDbContext : IEfCoreDbContext
+    where TEntity : class, IEntity<Guid>
+{
+    public PolpAbpEfCoreRepository(IDbContextProvider<TDbContext> dbContextProvider) : base(dbContextProvider)
+    {
+    }
+
+    /// <summary>
+    /// Performs an operation for adding/removing the child items into/from the parent entity.
+    /// </summary>
+    /// <typeparam name="TProperty">Typeof the the child item</typeparam>
+    /// <param name="id">Parent Id</param>
+    /// <param name="navPath">Expression for defining the child items</param>
+    /// <param name="func">A lambda function for adding or removing the child items</param>
+    /// <param name="autoSave">Auto save flag</param>
+    /// <param name="cancellationToken">Token</param>
+    /// <returns>Task</returns>
+    /// <exception cref="ArgumentNullException">If the given entity is not found</exception>
+    protected async Task AddOrRemoveChildItemsAsync<TProperty>(Guid id,
+        Expression<Func<TEntity, TProperty>> navPath,
+        Action<TEntity> func,
+        bool autoSave = false,
+        CancellationToken cancellationToken = default)
+    {
+        var context = await GetDbContextAsync();
+        var entity = context.Set<TEntity>().Include(navPath).First(b => b.Id == id);
+        if (entity == null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+        func(entity);
+        if (autoSave)
+        {
+            await context.SaveChangesAsync(GetCancellationToken(cancellationToken));
+        }
+    }
+
+    /// <summary>
+    /// Performs an operation for updating the child item
+    /// </summary>
+    /// <typeparam name="TProperty">Typeof the the child item</typeparam>
+    /// <param name="id">Parent Id</param>
+    /// <param name="navPath">Expression for defining the child items</param>
+    /// <param name="func">A lambda function for adding or removing the child items</param>
+    /// <param name="autoSave">Auto save flag</param>
+    /// <param name="cancellationToken">Token</param>
+    /// <returns>Task</returns>
+    /// <exception cref="ArgumentNullException">If the given entity is not found</exception>
+    protected async Task UpdateChildItemAsync<TProperty>(Guid id,
+        Expression<Func<TEntity, TProperty>> navPath,
+        Action<TEntity> func,
+        bool autoSave = false,
+        CancellationToken cancellationToken = default)
+    {
+        var context = await GetDbContextAsync();
+        var entity = context.Set<TEntity>().Include(navPath).First(b => b.Id == id);
+        if (entity == null)
+        {
+            throw new ArgumentNullException(nameof(entity));
+        }
+        func(entity);
+        context.Update(entity);
+        if (autoSave)
+        {
+            await context.SaveChangesAsync(GetCancellationToken(cancellationToken));
+        }
+    }
+}
+
---
+++ src/PolpAbp.Extensions.EntityFrameworkCore/PolpAbpExtensionsEntityFrameworkCoreModule.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Modularity;
+
+namespace PolpAbp.Extensions.EntityFrameworkCore
+{
+    [DependsOn(
+       typeof(AbpEntityFrameworkCoreModule)
+       )]
+    public class PolpAbpExtensionsEntityFrameworkCoreModule : AbpModule
+    {
+    }
+}
