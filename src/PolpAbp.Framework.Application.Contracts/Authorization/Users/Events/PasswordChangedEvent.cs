6eab69619e0a6045e9e4bb61e390d86910e43bc4
X. Tang <x.long@infobeyondtech.com>
2023-01-11 17:42:39 -0500

    Factor some local events into this module

---
+++ src/PolpAbp.Framework.Application.Contracts/Authorization/Users/Events/PasswordChangedEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Data;
+
+namespace PolpAbp.Framework.Authorization.Users.Events
+{
+    public class PasswordChangedEvent : IHasExtraProperties
+    {
+        public Guid? TenantId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid? OperatorId { get; set; }
+        public string NewPassword { get; set; }
+
+        public ExtraPropertyDictionary ExtraProperties { get; set; }
+
+        public PasswordChangedEvent()
+        {
+            ExtraProperties = new ExtraPropertyDictionary();
+        }
+    }
+}
---
+++ src/PolpAbp.Framework.Application.Contracts/Authorization/Users/Events/ProfileChangedEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Data;
+
+namespace PolpAbp.Framework.Authorization.Users.Events
+{
+    public class ProfileChangedEvent : IHasExtraProperties
+    {
+        public Guid? TenantId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid? OperatorId { get; set; }
+        public List<string> ChangedFields { get; set; }
+        public bool IsNew { get; set; }
+        public bool SendActivationEmail { get; set; }
+
+        public ExtraPropertyDictionary ExtraProperties {get;set; }
+
+        public ProfileChangedEvent()
+        {
+            ChangedFields = new List<string>();
+            ExtraProperties = new ExtraPropertyDictionary();
+        }
+    }
+}
