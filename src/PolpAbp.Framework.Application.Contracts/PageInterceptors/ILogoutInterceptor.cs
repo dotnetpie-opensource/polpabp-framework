3bf8e8de0c935f1ecdc7a279882a63878e440c6e
dotnetpie <tom@fake.com>
2023-01-12 15:23:12 -0500

    Introduce page interceptors

---
+++ src/PolpAbp.Framework.Application.Contracts/PageInterceptors/ILogoutInterceptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PolpAbp.Framework.PageInterceptors
+{
+	public interface ILogoutInterceptor
+	{
+		Task BeforeLogoutAsync();
+		Task AfterLogoutAsync();
+	}
+}
+
---
+++ src/PolpAbp.Framework.Application/PageInterceptors/NullLogoutInterceptor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace PolpAbp.Framework.PageInterceptors
+{
+    public class NullLogoutInterceptor : ILogoutInterceptor, ITransientDependency
+    {
+        public Task AfterLogoutAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task BeforeLogoutAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
+
