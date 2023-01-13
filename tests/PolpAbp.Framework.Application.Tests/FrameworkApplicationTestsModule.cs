2def851f2b27470f6ac6dcb0d283561e9f081fb6
Tom Tang <polpware@gmail.com>
2023-01-13 05:47:03 -0500

    Fix testing bugs as the result of shared temp key file

--- tests/PolpAbp.Framework.Application.Tests/FrameworkApplicationTestsModule.cs
+++ tests/PolpAbp.Framework.Application.Tests/FrameworkApplicationTestsModule.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using Volo.Abp.IdentityServer;
 using Volo.Abp.Modularity;
 using Volo.Abp.TenantManagement;
 
@@ -11,6 +13,19 @@ namespace PolpAbp.Framework
         )]
     public class FrameworkApplicationTestsModule : AbpModule
     {
+        public override void PreConfigureServices(ServiceConfigurationContext context)
+        {
+            PreConfigure<AbpIdentityServerBuilderOptions>(options =>
+            {
+                options.AddDeveloperSigningCredential = false;
+            });
+
+            PreConfigure<IIdentityServerBuilder>(identityServerBuilder =>
+            {
+                identityServerBuilder.AddDeveloperSigningCredential(persistKey: false, filename: Guid.NewGuid().ToString());
+            });
+        }
+
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
             context.Services.AddAlwaysAllowAuthorization();
