492dd46fa2e36a5b534d9160dd4a1f63fa18021a
Tom Tang <polpware@gmail.com>
2023-01-27 07:29:27 -0500

    Extensions to setting manager

--- src/PolpAbp.Framework.AbpExtensions.Settings/SettingManagementExtensions.cs
+++ src/PolpAbp.Framework.AbpExtensions.Settings/SettingManagementExtensions.cs
@@ -4,6 +4,39 @@ namespace Volo.Abp.SettingManagement
 {
     public static class SettingManagementExtensions
     {
+        public static async Task<T> GetDefaultAsync<T>([NotNull] this ISettingManager settingManager,
+            [NotNull] string name, bool fallback = true, T defaultValue = default)
+    where T : struct
+        {
+            Check.NotNull(settingManager, nameof(settingManager));
+            Check.NotNull(name, nameof(name));
+
+            var value = await settingManager.GetOrNullDefaultAsync(name, fallback);
+            return value?.To<T>() ?? defaultValue;
+        }
+
+        public static async Task<T> GetGlobalAsync<T>([NotNull] this ISettingManager settingManager,
+            [NotNull] string name, bool fallback = true, T defaultValue = default)
+    where T : struct
+        {
+            Check.NotNull(settingManager, nameof(settingManager));
+            Check.NotNull(name, nameof(name));
+
+            var value = await settingManager.GetOrNullGlobalAsync(name, fallback);
+            return value?.To<T>() ?? defaultValue;
+        }
+
+        public static async Task<T> GetConfigurationAsyncAsync<T>([NotNull] this ISettingManager settingManager,
+            [NotNull] string name, bool fallback = true, T defaultValue = default)
+    where T : struct
+        {
+            Check.NotNull(settingManager, nameof(settingManager));
+            Check.NotNull(name, nameof(name));
+
+            var value = await settingManager.GetOrNullConfigurationAsync(name, fallback);
+            return value?.To<T>() ?? defaultValue;
+        }
+
         public static async Task<T> GetForCurrentTenantAsync<T>([NotNull] this ISettingManager settingManager, 
             [NotNull] string name, bool fallback = true, T defaultValue = default)
     where T : struct
