c8584dc6e1b88ea5784d44da53016f774c0c6692
Tom Tang <polpware@gmail.com>
2023-03-23 22:13:34 -0400

    Read token from query parameter

---
+++ src/PolpAbp.Framework.AspNetCore/Infrastructure/AttemptReadTokenFromQueryMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace PolpAbp.Framework.Infrastructure
+{
+    public class AttemptReadTokenFromQueryMiddleware : IMiddleware, ITransientDependency
+    {
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            // The case-insensitive query parameter will normalize the token into a lowere case.
+            var tokenValue = context.Request.Query["EncodedToken"].FirstOrDefault();
+
+            if (tokenValue != null)
+            {
+                // Set up the authorization header
+                context.Request.Headers["Authorization"] = "Bearer " + tokenValue;
+            }
+            await next(context).ConfigureAwait(false);
+        }
+    }
+}
