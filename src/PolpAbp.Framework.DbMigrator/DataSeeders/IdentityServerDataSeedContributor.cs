698c238257733425872b8acbf13ccbe09260fa5c
Tom Tang <polpware@gmail.com>
2023-04-09 18:21:13 -0400

    Improve identity server data seeder

--- src/PolpAbp.Framework.DbMigrator/DataSeeders/IdentityServerDataSeedContributor.cs
+++ src/PolpAbp.Framework.DbMigrator/DataSeeders/IdentityServerDataSeedContributor.cs
@@ -4,42 +4,36 @@ using Microsoft.Extensions.Configuration;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Guids;
+using Volo.Abp.IdentityServer.ApiScopes;
 using Volo.Abp.IdentityServer.ApiResources;
 using Volo.Abp.IdentityServer.Clients;
 using Volo.Abp.IdentityServer.IdentityResources;
 using Volo.Abp.Uow;
+using ApiScope = Volo.Abp.IdentityServer.ApiScopes.ApiScope;
+using ApiResource = Volo.Abp.IdentityServer.ApiResources.ApiResource;
 using Client = Volo.Abp.IdentityServer.Clients.Client;
 
 namespace PolpAbp.Framework.DataSeeders;
 
 public class IdentityServerDataSeedContributor : IDataSeedContributor, ITransientDependency
 {
+    private readonly IApiScopeRepository _apiScopeRepository;
     private readonly IApiResourceRepository _apiResourceRepository;
     private readonly IClientRepository _clientRepository;
     private readonly IIdentityResourceDataSeeder _identityResourceDataSeeder;
     private readonly IGuidGenerator _guidGenerator;
     private readonly IConfiguration _configuration;
 
-    string[] CommonScopes = new[]
-          {
-            "openid",
-            "email",
-            "phone",
-            "profile",
-            "role",
-            "address",
-            "GoGoCite",
-            "offline_access"
-        };
-
     public IdentityServerDataSeedContributor(
         IClientRepository clientRepository,
+        IApiScopeRepository apiScopeRepository,
         IApiResourceRepository apiResourceRepository,
         IIdentityResourceDataSeeder identityResourceDataSeeder,
         IGuidGenerator guidGenerator,
         IConfiguration configuration)
     {
         _clientRepository = clientRepository;
+        _apiScopeRepository = apiScopeRepository;
         _apiResourceRepository = apiResourceRepository;
         _identityResourceDataSeeder = identityResourceDataSeeder;
         _guidGenerator = guidGenerator;
@@ -50,10 +44,143 @@ public class IdentityServerDataSeedContributor : IDataSeedContributor, ITransien
     public virtual async Task SeedAsync(DataSeedContext context)
     {
         await _identityResourceDataSeeder.CreateStandardResourcesAsync();
-        await UpdateClientsAsync();
+        await PopulateApiScopesAsync();
+        await PopulateApiResourcesAsync();
+        await PopulateClientsAsync();
     }
 
-    private async Task UpdateClientsAsync()
+    #region ApiScopes
+
+    private async Task PopulateApiScopesAsync()
+    {
+        var configurationSection = _configuration.GetSection("IdentityServer:ApiScopes");
+        var mappings = new Dictionary<string, ApiScopeModel>();
+        configurationSection.Bind(mappings);
+
+        foreach (var mapping in mappings)
+        {
+            if (mapping.Value.IsDeleted)
+            {
+                await DeleteApiScopeAsync(mapping.Key);
+            }
+            else
+            {
+                await CreateApiScopeAsync(mapping.Key, mapping.Value);
+            }
+        }
+    }
+
+    private async Task<ApiScope> CreateApiScopeAsync(string name, ApiScopeModel model)
+    {
+        var apiScope = await _apiScopeRepository.FindByNameAsync(name);
+        if (apiScope != null)
+        {
+            return apiScope;
+        }
+
+        if (apiScope == null)
+        {
+            apiScope = await _apiScopeRepository.InsertAsync(
+                new ApiScope(
+                    _guidGenerator.Create(),
+                    name,
+                    model.DisplayName ?? name
+                ),
+                autoSave: true
+            );
+        }
+
+        foreach (var claim in model.UserClaims)
+        {
+            if (apiScope.FindClaim(claim) == null)
+            {
+                apiScope.AddUserClaim(claim);
+            }
+        }
+
+        return await _apiScopeRepository.UpdateAsync(apiScope);
+    }
+
+    private async Task DeleteApiScopeAsync(string name)
+    {
+        var apiScope = await _apiScopeRepository.FindByNameAsync(name);
+        if (apiScope != null)
+        {
+            await _apiScopeRepository.DeleteAsync(apiScope, true);
+        }
+    }
+    #endregion
+
+    #region ApiResources
+    private async Task PopulateApiResourcesAsync()
+    {
+        var configurationSection = _configuration.GetSection("IdentityServer:ApiResources");
+        var mappings = new Dictionary<string, ApiResourceModel>();
+        configurationSection.Bind(mappings);
+
+        foreach (var mapping in mappings)
+        {
+            if (mapping.Value.IsDeleted)
+            {
+                await DeleteApiResourceAsync(mapping.Key);
+            }
+            else
+            {
+                await CreateApiResourceAsync(mapping.Key, mapping.Value);
+            }
+        }
+    }
+
+    private async Task<ApiResource> CreateApiResourceAsync(string name, ApiResourceModel model)
+    {
+        var apiResource = await _apiResourceRepository.FindByNameAsync(name);
+        if (apiResource != null) {
+            return apiResource;
+        }
+
+        if (apiResource == null)
+        {
+            apiResource = await _apiResourceRepository.InsertAsync(
+                new ApiResource(
+                    _guidGenerator.Create(),
+                    name,
+                    model.DisplayName ?? name + " API"
+                ),
+                autoSave: true
+            );
+        }
+
+        foreach (var claim in model.UserClaims)
+        {
+            if (apiResource.FindClaim(claim) == null)
+            {
+                apiResource.AddUserClaim(claim);
+            }
+        }
+
+        foreach(var s in model.Scopes)
+        {
+            if (apiResource.FindScope(s) == null)
+            {
+                apiResource.AddScope(s);
+            }
+        }
+
+        return await _apiResourceRepository.UpdateAsync(apiResource);
+    }
+
+    private async Task DeleteApiResourceAsync(string name)
+    {
+        var apiResource = await _apiResourceRepository.FindByNameAsync(name);
+        if (apiResource != null)
+        {
+            await _apiResourceRepository.DeleteAsync(apiResource, true);
+        }
+    }
+    #endregion
+
+    #region Clients
+    private async Task PopulateClientsAsync()
     {
         var configurationSection = _configuration.GetSection("IdentityServer:Clients");
         var mappings = new Dictionary<string, ClientModel>();
@@ -106,12 +233,13 @@ public class IdentityServerDataSeedContributor : IDataSeedContributor, ITransien
                 IdentityTokenLifetime = clientModel.IdentityTokenLifetime,
                 SlidingRefreshTokenLifetime = clientModel.SlidingRefreshTokenLifetime,
                 RequireConsent = clientModel.RequireConsent,
-                RequireClientSecret = clientModel.RequireClientSecret
+                RequireClientSecret = clientModel.RequireClientSecret,
+                RequirePkce = clientModel.RequirePkce
             },
             autoSave: true
         );
 
-        foreach (var scope in CommonScopes)
+        foreach (var scope in clientModel.Scopes)
         {
             if (client.FindScope(scope) == null)
             {
@@ -159,6 +287,31 @@ public class IdentityServerDataSeedContributor : IDataSeedContributor, ITransien
 
         return await _clientRepository.UpdateAsync(client);
     }
+    #endregion
+
+    class ApiScopeModel
+    {
+        public bool IsDeleted { get; set; }
+        public string DisplayName { get; set; }
+        public List<string> UserClaims { get; set; }
+
+        public ApiScopeModel()
+        {
+            UserClaims = new List<string>();
+        }
+    }
+
+    class ApiResourceModel {
+        public bool IsDeleted { get; set; }
+        public string DisplayName { get; set; }
+        public List<string> UserClaims { get; set; }
+        public List<string> Scopes { get; set; }
+
+        public ApiResourceModel() {
+            UserClaims = new List<string>();
+            Scopes = new List<string>();
+        }
+    }
 
     class ClientModel
     {
@@ -170,8 +323,10 @@ public class IdentityServerDataSeedContributor : IDataSeedContributor, ITransien
         public List<string> CorsOrigins { get; set; }
         public List<string> PostLogoutRedirectUris { get; set; }
         public List<string> RedirectUris { get; set; }
+        public List<string> Scopes { get;set; }
         public bool RequireConsent { get; set; }
         public bool RequireClientSecret { get; set; }
+        public bool RequirePkce { get; set; }
 
         public int IdentityTokenLifetime {get; set; }
         public int AccessTokenLifetime { get; set; }
@@ -185,8 +340,10 @@ public class IdentityServerDataSeedContributor : IDataSeedContributor, ITransien
             CorsOrigins = new List<string>();
             PostLogoutRedirectUris = new List<string>();
             RedirectUris = new List<string>();
+            Scopes = new List<string>();
             RequireConsent = false;
             RequireClientSecret = false;
+            RequirePkce = false;
             IdentityTokenLifetime = 300; // 5 min
             AccessTokenLifetime = 3600; // 1 hour
             AuthorizationCodeLifetime = 300; // 5 min
