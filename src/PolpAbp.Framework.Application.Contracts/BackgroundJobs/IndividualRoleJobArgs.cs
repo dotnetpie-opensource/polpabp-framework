01560b4903072648d8ae61316dda30a477947a18
dotnetpie <tom@fake.com>
2023-06-16 10:36:32 -0400

    New feature: Base class for role job args

---
+++ src/PolpAbp.Framework.Application.Contracts/BackgroundJobs/IndividualRoleJobArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PolpAbp.Framework.BackgroundJobs
+{
+    public abstract class IndividualRoleJobArgs
+    {
+        public Guid? TenantId { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public Guid? OperatorId { get; set; }
+
+    }
+}
