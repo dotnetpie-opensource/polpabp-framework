1af832772fc47eb1eed263402e3c29c5e7df9c28
dotnetpie <tom@fake.com>
2023-06-22 17:24:51 -0400

    Introduce state change ETO

---
+++ src/PolpAbp.Framework.Events/DistributedEvents/Account/AccountStateChangeEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PolpAbp.Framework.DistributedEvents.Account
+{
+	public enum AccountStateChangeEnum
+    {
+       RegisteredOnItsOwn = 10,
+       ActivatedOnItsOwn = 20,
+       ApprovedByAdmin = 30,
+       DeactivatedByAdmin = 40
+	}
+}
+
---
+++ src/PolpAbp.Framework.Events/DistributedEvents/Account/AccountStateChangeEto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.EventBus;
+
+namespace PolpAbp.Framework.DistributedEvents.Account
+{
+    [EventName("PolpAbp.Framework.DistributedEvents.Account.StateChange")]
+    public class AccountStateChangeEto
+	{
+        public Guid TenantId { get; set; }
+        public Guid AccountId { get; set; }
+        public AccountStateChangeEnum ChangeId { get; set; }
+    }
+}
+
