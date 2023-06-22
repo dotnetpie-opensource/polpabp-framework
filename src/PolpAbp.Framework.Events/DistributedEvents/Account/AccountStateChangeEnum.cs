31556a8b33019978ef9d9b52cd71b7ae0ec3833e
dotnetpie <tom@fake.com>
2023-06-22 17:39:57 -0400

    Extend account state change events

--- src/PolpAbp.Framework.Events/DistributedEvents/Account/AccountStateChangeEnum.cs
+++ src/PolpAbp.Framework.Events/DistributedEvents/Account/AccountStateChangeEnum.cs
@@ -6,7 +6,10 @@ namespace PolpAbp.Framework.DistributedEvents.Account
        RegisteredOnItsOwn = 10,
        ActivatedOnItsOwn = 20,
        ApprovedByAdmin = 30,
-       DeactivatedByAdmin = 40
+       DeactivatedByAdmin = 40,
+       CreatedByAdmin = 50,
+       DeletedByAdmin = 60,
+       TerminatedOnItsOwn = 70
 	}
 }
 
