661775b2f68b46f134562ea9b2303e3f1b009286
dotnetpie <tom@fake.com>
2023-09-11 10:18:23 -0400

    Added the tests for audit logging repository

---
+++ tests/PolpAbp.Framework.EntityFrameworkCore.Tests/EntityFrameworkCore/AuditLogging/AuditLogRepositoryExtTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PolpAbp.Framework.AuditLogging;
+
+namespace PolpAbp.Framework.EntityFrameworkCore.AuditLogging
+{
+	public class AuditLogRepositoryExtTests : FrameworkEntityFrameworkCoreTestBase {
+
+        private readonly IAuditLogRepositoryExt _repo;
+
+        public AuditLogRepositoryExtTests()
+        {
+            _repo = GetRequiredService<IAuditLogRepositoryExt>();
+        }
+
+        public 
+    }
+}
+
